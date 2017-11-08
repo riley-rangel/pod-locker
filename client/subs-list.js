315fd7ac11194b1668c4ed94a5a9483cc22958e8
Riley Rangel <rrangel91@gmail.com>
2017-11-08 15:56:45 -0800

    Define a function using React JSX to return a SubsList which takes a subs prop. #2

---
+++ client/subs-list.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Grid from 'material-ui/Grid'
+
+export default function SubsList(subs) {
+  return (
+    <Grid container>
+      <Grid item xs={ 12 } md={ 6 }>
+      </Grid>
+    </Grid>
+  )
+}
