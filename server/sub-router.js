9ff421ecb47c83206bab33e7ded3b5ccb05c6b43
Riley Rangel <rrangel91@gmail.com>
2017-11-08 13:28:33 -0800

    Define a GET route on sub-router.js which utilizes the find method to request all subs. #2

--- server/sub-router.js
+++ server/sub-router.js
@@ -6,21 +6,31 @@ const handleError = require('./handle-error')
 module.exports = function subRouter(gateway) {
   const router = new Router()
 
-  router.post('/subscribe', (req, res) => {
-    const { feed } = req.body
-    rssParser.parseURL(feed, async (err, parsed) => {
-      if (err) handleError(err, res, 400, 1)
-      const converted = feedConverter(parsed.feed)
+  router
+    .get('/subscriptions', async (req, res) => {
       try {
-        const podcast = { about: converted.about, feed }
-        await gateway.subscribe(podcast)
-        res.status(202).json(podcast)
+        const subs = await gateway.find()
+        res.status(200).json(subs)
       }
       catch (err) {
         handleError(err, res, 500)
       }
     })
-  })
+    .post('/subscribe', (req, res) => {
+      const { feed } = req.body
+      rssParser.parseURL(feed, async (err, parsed) => {
+        if (err) handleError(err, res, 400, 1)
+        const converted = feedConverter(parsed.feed)
+        try {
+          const podcast = { about: converted.about, feed }
+          await gateway.subscribe(podcast)
+          res.status(202).json(podcast)
+        }
+        catch (err) {
+          handleError(err, res, 500)
+        }
+      })
+    })
 
   return router
 }
