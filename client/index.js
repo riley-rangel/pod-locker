7d6d95c53f8cacb536c28fb88e2936587d3687db
Riley Rangel <rrangel91@gmail.com>
2017-11-10 10:19:21 -0800

    Refactor app.js to import and use container components. #3

--- client/index.js
+++ client/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { App } from './app.js'
+import App from './app.js'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 
 injectTapEventPlugin()
